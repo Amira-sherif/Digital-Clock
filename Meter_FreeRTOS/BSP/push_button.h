d427c10505fc01271ae61b35c449134142ddee52
Amira Sherif <amira.mohammed00@eng-st.cu.edu.eg>
2023-06-16 21:58:13 +0300

    commit message

---
+++ Meter_FreeRTOS/BSP/push_button.h
@@ -0,0 +1,16 @@
+#ifndef __PB_H__
+#define __PB_H__
+
+/* Define push button task priority */
+/* Add your code here!: DONE! */
+#define PB_PRIORITY		(2)
+/* End of your code! */
+
+/* Define push button task period in MS */
+/* Add your code here!: DONE! */
+#define PB_PERIOD_MS	(20)
+/* End of your code! */
+
+void PB_Init(void);
+
+#endif
---
+++ Meter_FreeRTOS/display.h
@@ -0,0 +1,15 @@
+#ifndef __DISPLAY_H__
+#define __DISPLAY_H__
+
+/* Define display task priority */
+/* Add your code here! */
+#define DISP_PPRIORITY		(1)
+/* End of your code! */
+
+void DISP_Init(void);
+
+
+
+
+
+#endif
